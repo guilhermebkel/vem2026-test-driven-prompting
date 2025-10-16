061c7dfd12d49e195e691a153d0424a75797e650
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2025-10-16 14:28:29 -0300

    chore(context): add private functions to handle

--- src/Services/ContextService.ts
+++ src/Services/ContextService.ts
@@ -5,10 +5,10 @@ import FileUtil from "@/Utils/FileUtil"
 class ContextService {
 	async buildContext(contextDefinition: ContextDefinition): Promise<BuildedContext> {
 		const contextSlugToContentHandlerFn: Record<ContextSlug, (contextItem: ContextDefinitionItem) => Promise<string>> = {
-			"typing": async (contextItem) => await FileUtil.getFileContent(contextItem.path!),
-			"project-metadata": async (contextItem) => await FileUtil.getFileContent(contextItem.path!),
-			"repository-root-structure": async (contextItem) => (await FileUtil.getFolderPathList(contextItem.path!)).join("\n"),
-			"similar-method": async (contextItem) => await FileUtil.getFileContent(contextItem.path!)
+			"typing": async (contextItem) => await this.handleContentDefault(contextItem),
+			"project-metadata": async (contextItem) => await this.handleContentDefault(contextItem),
+			"repository-root-structure": async (contextItem) => await this.handleContentFolder(contextItem),
+			"similar-method": async (contextItem) => await this.handleContentDefault(contextItem)
 		}
 
 		const buildedContext: BuildedContext = await Promise.all(
@@ -24,6 +24,24 @@ class ContextService {
 		)
 
 		return buildedContext
+	}
+
+	private async handleContentDefault(contextItem: ContextDefinitionItem): Promise<string> {
+		if (contextItem.content) {
+			return contextItem.content
+		}
+
+		if (contextItem.path) {
+			return await FileUtil.getFileContent(contextItem.path)
+		}
+
+		return "N/A"
+	}
+
+	private async handleContentFolder(contextItem: ContextDefinitionItem): Promise<string> {
+		const folderPathList = await FileUtil.getFolderPathList(contextItem.path!)
+
+		return folderPathList.join("\n")
 	}
 }
 
