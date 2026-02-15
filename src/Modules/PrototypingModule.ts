720e526e97e8deb7b400c08f2df43f39da6a1d66
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2026-02-15 15:40:57 -0300

    chore(*): get methods

--- src/Modules/PrototypingModule.ts
+++ src/Modules/PrototypingModule.ts
@@ -1,11 +1,21 @@
+import { ExploreMethodResult } from "@/Protocols/MethodExplorerProtocol"
 import {
 	PrototypeOptions,
 	PrototypeResult
 } from "@/Protocols/PrototypingProtocol"
 
+import LogService from "@/Services/LogService"
+import FileUtil from "@/Utils/FileUtil"
+
 class PrototypingModule {
 	async prototype(options: PrototypeOptions): Promise<PrototypeResult> {
-		console.log(options)
+		const methodExplorationResultLogFilePath = LogService.getMethodExplorationResultLogFilePath(options.repositoryName)
+		const methodExplorationResultLogFileContent = await FileUtil.getFileContent(methodExplorationResultLogFilePath)
+		const exploreMethodResult: ExploreMethodResult = JSON.parse(methodExplorationResultLogFileContent)
+
+		const resolvedTestFilePaths = exploreMethodResult.map(result => result.resolvedMethodFilePath).flat()
+		const uniqueResolvedTestFilePaths = [...new Set(resolvedTestFilePaths)]
+		console.log(uniqueResolvedTestFilePaths)
 	}
 }
 
