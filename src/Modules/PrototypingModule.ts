474e403c8526032a6fe8c94bf2d42415b27c15b5
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2026-03-01 19:10:54 -0300

    chore(teste-relevance): add new kind of score

--- src/Modules/PrototypingModule.ts
+++ src/Modules/PrototypingModule.ts
@@ -127,7 +127,7 @@ class PrototypingModule {
 								result.testCaseName === testCaseRelevance.name
 							))
 
-							testCaseRelevance.dataFlowScore = testDataFlowResultForTestCase?.heuristics?.directExpectUse ? "relevant" : "not-relevant"
+							testCaseRelevance.dataFlowScore = Object.values(testDataFlowResultForTestCase?.heuristics || {}).some(Boolean) ? "relevant" : "not-relevant"
 						}
 
 						return testCaseRelevance
