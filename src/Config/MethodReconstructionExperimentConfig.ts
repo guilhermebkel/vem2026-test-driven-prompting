dd59e6851dd6b5cb5f009a5e6aa1a9ed10e27303
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2026-04-18 21:14:20 -0300

    chore(*): add initial run

--- src/Config/MethodReconstructionExperimentConfig.ts
+++ src/Config/MethodReconstructionExperimentConfig.ts
@@ -4,6 +4,21 @@ import { ExperimentComparison } from "@/Protocols/MethodReconstructionExperiment
 
 const DEFAULT_EXPERIMENT_COMPARISONS: ExperimentComparison[] = [
 	{
+		title: "gemini-2.5-flash|temperature-0|all-relevant-test-case",
+		model: { name: "gemini-2.5-flash", reasoningBudget: 0, temperature: 0 },
+		context: { definitions: [{ slug: "relevant-test-case" }], isPermutationEnabled: false }
+	},
+	{
+		title: "gemini-2.5-flash|temperature-0|permuted-relevant-test-case",
+		model: { name: "gemini-2.5-flash", reasoningBudget: 0, temperature: 0 },
+		context: { definitions: [{ slug: "relevant-test-case" }], isPermutationEnabled: true }
+	},
+	{
+		title: "gemini-2.5-flash|temperature-0|no-relevant-test-case",
+		model: { name: "gemini-2.5-flash", reasoningBudget: 0, temperature: 0 },
+		context: { definitions: [], isPermutationEnabled: false }
+	},
+	{
 		title: "gemini-3.0-flash|temperature-0|all-relevant-test-case",
 		model: { name: "gemini-3.0-flash", reasoningBudget: 0, temperature: 0 },
 		context: { definitions: [{ slug: "relevant-test-case" }], isPermutationEnabled: false }
@@ -24,10 +39,10 @@ export const METHOD_FILE_PATH_PLACEHOLDER = "<method_file_path>"
 
 export const methodReconstructionExperimentValidation = {
 	hasMinimumContextCount: (exploredContext?: ExploredContext): boolean => (
-		Number(exploredContext?.context?.length) >= 3
+		Number(exploredContext?.context?.length) >= 5
 	),
 	hasReachedMaximumMethodExperimentedCount: (methodExperimentedCount: number): boolean => (
-		methodExperimentedCount >= 2
+		methodExperimentedCount >= 5
 	)
 }
 
